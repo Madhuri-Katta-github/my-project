d70ba42e574fcac4773c93b1feb979793796ce8f
Madhuri-Katta-github <madhurikatta717@gmail.com>
2024-08-01 14:19:50 +0530

    server and db is connected for backend

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => console.log("Mongo db is connected"))
+.catch((error) => console.log(error));
+
+const app = express();
+
+app.listen(3000, () => { console.log("server is connected!!") });
+
+export default app;
