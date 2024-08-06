ee5bd7ec1d90728ee6a30414b912e812d433ae37
Madhuri-Katta-github <madhurikatta717@gmail.com>
2024-08-06 13:10:08 +0530

    created signup backend api

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+import User from "../models/user.model.js"
+import { errorHandler } from "../utils/error.js";
+
+export const signup = async (req, res, next) => {
+  console.log(req.body);
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password || username === "" || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"))
+  }
+
+  const newUser = new User({
+    username,
+    email,
+    password
+  });
+
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    next(error)
+  }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,33 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js'
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
-.then(() => console.log("Mongo db is connected"))
-.catch((error) => console.log(error));
+    .then(() => console.log("Mongo db is connected"))
+    .catch((error) => console.log(error));
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => { console.log("server is connected!!") });
+
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+
+//creating middleware to handle errors
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error'
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+});
 
 export default app;
