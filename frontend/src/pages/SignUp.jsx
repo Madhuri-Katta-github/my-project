4234fa9a23be7c14ba9d695493725c1e8c29fa8a
Madhuri-Katta-github <madhurikatta717@gmail.com>
2024-08-07 12:09:35 +0530

    user signup functionality

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,30 +1,85 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Button, Label, TextInput, Spinner, Alert } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  // console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success == false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error);
+      setLoading(false);
+    }
+  };
+
   return (
-    <form className="flex max-w-md flex-col gap-4 mx-auto mt-8">
+    <form
+      className="flex max-w-md flex-col gap-4 mx-auto mt-8"
+      onSubmit={handleSubmit}
+    >
       <div>
         <div className="mb-2 block">
           <Label htmlFor="username" value="Username" />
         </div>
-        <TextInput id="username" type="text" required />
+        <TextInput id="username" type="text" required onChange={handleChange} />
       </div>
       <div>
         <div className="mb-2 block">
           <Label htmlFor="email" value="Email" />
         </div>
-        <TextInput id="email" type="email" required />
+        <TextInput id="email" type="email" required onChange={handleChange} />
       </div>
       <div>
         <div className="mb-2 block">
           <Label htmlFor="password" value="Password" />
         </div>
-        <TextInput id="password" type="password" required />
+        <TextInput
+          id="password"
+          type="password"
+          required
+          onChange={handleChange}
+        />
       </div>
 
       <Button type="submit" gradientDuoTone="redToYellow">
-        Sign Up
+        {loading ? (
+          <>
+            <Spinner size={"sm"} />
+            <span className="pl-3">Loading...</span>
+          </>
+        ) : (
+          "Sign Up"
+        )}
       </Button>
       <div className="flex gap-2 text-sm mt-5">
         <span>Do Have an account?</span>
@@ -32,6 +87,11 @@ export default function SignUp() {
           Sign In
         </Link>
       </div>
+      {errorMessage && (
+        <Alert className="mt-5" color="failure">
+          {errorMessage}
+        </Alert>
+      )}
     </form>
   );
 }
