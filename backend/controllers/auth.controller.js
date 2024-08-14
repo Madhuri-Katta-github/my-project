2be9527f844cf804147a679a718b78e80eb00a38
Madhuri-Katta-github <madhurikatta717@gmail.com>
2024-08-14 15:42:23 +0530

    signin backend + ui funcionality done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js"
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 
 export const signup = async (req, res, next) => {
   console.log(req.body);
@@ -10,12 +12,12 @@ export const signup = async (req, res, next) => {
     next(errorHandler(400, "All fields are required"))
   }
 
-  const hashedPassword=bcryptjs.hashSync(password,10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
     username,
     email,
-    password:hashedPassword
+    password: hashedPassword
   });
 
   try {
@@ -23,5 +25,39 @@ export const signup = async (req, res, next) => {
     res.json('Signup successful');
   } catch (error) {
     next(error)
+  }
+}
+
+export const signIn = async (req, res, next) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  if (email === "" || password === "" || !email || !password) {
+    next(errorHandler(400, "All fields are required"))
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser.email) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password"));
+    }
+
+    const token = jwt.sign({ id: validPassword._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc
+
+    res.status(200).cookie('access_token', token, {
+      httpOnly: true,
+    }).json(rest);
+
+  } catch (error) {
+    next(error);
   }
 }
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,31 +1,85 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, TextInput, Spinner } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      setErrorMessage("Please fill all the form details");
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        setErrorMessage(data.message);
+      }
+
+      if (res.ok) {
+        setErrorMessage(null);
+        setLoading(false);
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
   return (
-    <form className="flex max-w-md flex-col gap-4 mx-auto mt-8">
+    <form
+      className="flex max-w-md flex-col gap-4 mx-auto mt-8"
+      onSubmit={handleSubmit}
+    >
       <div>
         <div className="mb-2 block">
           <Label htmlFor="email" value="Email" />
         </div>
-        <TextInput id="email" type="email" required />
+        <TextInput id="email" type="email" required onChange={handleChange} />
       </div>
       <div>
         <div className="mb-2 block">
           <Label htmlFor="password" value="Password" />
         </div>
-        <TextInput id="password" type="password" required />
+        <TextInput
+          id="password"
+          type="password"
+          required
+          onChange={handleChange}
+        />
       </div>
-
       <Button type="submit" gradientDuoTone="redToYellow">
-        Sign In
+        {loading ? <Spinner /> : "Sign In"}
       </Button>
       <div className="flex gap-2 text-sm mt-5">
         <span>Dont Have an account?</span>
         <Link to="/sign-up" className="text-blue-500">
-          Sign In
+          Sign up
         </Link>
       </div>
+      {errorMessage && (
+        <Alert className="mt-5" color="failure">
+          {errorMessage}
+        </Alert>
+      )}
     </form>
   );
 }
